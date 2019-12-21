1c787f6ca52027826e3a48c35d8cc2b852d5fc41
Sabrina Akbar <sabrinaakbar141@gmail.com>
2019-12-21 14:34:58 -0600

    Finish writing the code for power of two (#1)

--- GitTest/main.cpp
+++ GitTest/main.cpp
@@ -2,10 +2,18 @@
 
 using namespace std;
 
+int PowerOfTwo(int y) {
+	int result = 0;
+	result = std::pow(2, y);
+	return result;
+}
+
 int main() {
 
 	cout << "Hellow World" << endl;
-
+	int input = 4;
+	int output = PowerOfTwo(input);
+	cout << "The result of power two is: " << output << endl;
 	system("pause");
 	return 0;
 }
